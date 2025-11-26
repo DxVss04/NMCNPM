1f17266e9286e4d51dfba7e61625a0d63ae6e2c9
DxVss04 <vinhtoc2004@gmail.com>
2025-11-26 09:21:27 +0700

    updateREADME

--- backend/routes/houseHoldRoute.js
+++ backend/routes/houseHoldRoute.js
@@ -1,8 +1,16 @@
-import { createHouseHold } from "../controllers/houseHoldControllers.js";
+import {
+  createHouseHold,
+  updateHouseHold,
+  getHouseHoldByHeadIdentification,
+} from "../controllers/houseHoldControllers.js";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/create", createHouseHold);
-
+router.post("/create-household", createHouseHold);
+router.patch("/update-hosehold", updateHouseHold);
+router.get(
+  "/read-household/:identification_head",
+  getHouseHoldByHeadIdentification
+);
 export default router;
