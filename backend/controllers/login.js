81d544de42a2380030a8d0d5945320edfd9194be
kostromin-s <nam.lh220118@sis.hust.edu.vn>
2025-12-22 16:10:10 +0700

    fix bug cho cac be

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,5 +1,4 @@
-import User from "../models/userModel";
-import { registerUser } from "./registerUser";
+import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
 export const login = async (req, res) => {
